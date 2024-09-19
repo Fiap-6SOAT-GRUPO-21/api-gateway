55531004037a587d69b47d08ef0f25c6e0d4849a
JoseVitorDurante <josevitor_limma@hotmail.com>
2024-09-19 09:50:28 -0300

    fmt

--- terraform/modules/api-gateway/api-gateway.tf
+++ terraform/modules/api-gateway/api-gateway.tf
@@ -8,18 +8,18 @@ resource "aws_security_group" "vpc_link" {
   vpc_id = var.vpc_id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # Create the VPC Link configured with the ALB subnets.
 resource "aws_apigatewayv2_vpc_link" "api_vpc_link" {
-  name               = "${var.project_name}-api_vpc_link"
+  name       = "${var.project_name}-api_vpc_link"
   security_group_ids = [aws_security_group.vpc_link.id]
-  subnet_ids         = var.private_subnet_ids
+  subnet_ids = var.private_subnet_ids
 }
 
 
@@ -38,13 +38,13 @@ resource "aws_apigatewayv2_stage" "api_gateway_stage" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
-  depends_on  = [aws_apigatewayv2_api.api]
+  depends_on = [aws_apigatewayv2_api.api]
 }
 
 # Search for the Load Balancer created by the K8s service for api-food micorservice
 data "aws_lb" "eks_api_food" {
   tags = {
-    "kubernetes.io/service-name" = "default/${var.lb_service_name_api_food}"
+    "kubernetes.io/service-name"                = "default/${var.lb_service_name_api_food}"
     "kubernetes.io/cluster/${var.project_name}" = "owned"
   }
 }
@@ -64,7 +64,8 @@ resource "aws_apigatewayv2_integration" "api_integration_api_food" {
   connection_type        = "VPC_LINK"
   connection_id          = aws_apigatewayv2_vpc_link.api_vpc_link.id
   payload_format_version = "1.0"
-  depends_on = [aws_apigatewayv2_vpc_link.api_vpc_link,
+  depends_on = [
+    aws_apigatewayv2_vpc_link.api_vpc_link,
     aws_apigatewayv2_api.api
   ]
 }
@@ -76,5 +77,5 @@ resource "aws_apigatewayv2_route" "api_gateway_route_api_food" {
   route_key          = "ANY /{proxy+}"
   target             = "integrations/${aws_apigatewayv2_integration.api_integration_api_food.id}"
   authorization_type = "NONE"
-  depends_on         = [aws_apigatewayv2_integration.api_integration_api_food]
+  depends_on = [aws_apigatewayv2_integration.api_integration_api_food]
 }
